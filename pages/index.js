5d0a0821172c3e88a9114b2b45f23eba38700fce
nascimentolds <47907106+nascimentolds@users.noreply.github.com>
2021-07-14 17:46:33 -0300

    Início do formulário

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import MainGrid from "../src/components/MainGrid";
 import Box from "../src/components/Box";
-import { AlurakutMenu, OrkutNostalgicIconSet } from "../src/lib/AluraCommons";
+import {
+  AlurakutMenu,
+  AlurakutProfileSidebarMenuDefault,
+  OrkutNostalgicIconSet,
+} from "../src/lib/AluraCommons";
 import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
 //const Title = styled.h1`
@@ -15,6 +19,17 @@ function ProfileSidebar(props) {
         src={`https://github.com/${props.guitHubUser}.png`}
         style={{ borderRadius: "8px" }}
       />
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.guitHubUser}`}>
+          @{props.guitHubUser}
+        </a>
+      </p>
+
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   );
 }
@@ -42,6 +57,32 @@ export default function Home() {
             <h1 className="title">Bem vindo(a)</h1>
 
             <OrkutNostalgicIconSet />
+          </Box>
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form
+              onSubmit={function handleCriarComunidade(e) {
+                e.preventDefault();
+                console.log(e);
+              }}
+            >
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+              <button>Criar comunidade</button>
+            </form>
           </Box>
         </div>
         <div
