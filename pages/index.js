bae5940a470996ad18f80984040a4920e183d882
nascimentolds <47907106+nascimentolds@users.noreply.github.com>
2021-07-16 11:30:16 -0300

    Formulário de comunidades

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import MainGrid from "../src/components/MainGrid";
 import Box from "../src/components/Box";
 import {
@@ -14,7 +15,7 @@ import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
 function ProfileSidebar(props) {
   return (
-    <Box>
+    <Box as="aside">
       <img
         src={`https://github.com/${props.guitHubUser}.png`}
         style={{ borderRadius: "8px" }}
@@ -36,6 +37,13 @@ function ProfileSidebar(props) {
 
 export default function Home() {
   const guitHubUser = "nascimentolds";
+  const [comunidades, setComunidades] = React.useState([
+    {
+      id: "123456",
+      title: "Eu odeio acordar cedo",
+      image: "https://alurakut.vercel.app/capa-comunidade-01.jpg",
+    },
+  ]);
   const pessoasFavoritas = [
     "juunegreiros",
     "omariosouto",
@@ -63,7 +71,18 @@ export default function Home() {
             <form
               onSubmit={function handleCriarComunidade(e) {
                 e.preventDefault();
-                console.log(e);
+                const dadosDoForm = new FormData(e.target);
+
+                const comunidade = {
+                  id: new Date().toISOString(),
+                  title: dadosDoForm.get("title"),
+                  image: dadosDoForm.get("image"),
+                };
+
+                //comunidades.push("Alura Again");
+                const comunidadesAtualizadas = [...comunidades, comunidade];
+                setComunidades(comunidadesAtualizadas);
+                console.log(comunidades);
               }}
             >
               <div>
@@ -97,10 +116,24 @@ export default function Home() {
             <ul>
               {pessoasFavoritas.map((itemAtual) => {
                 return (
-                  <li>
-                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                  <li key={itemAtual}>
+                    <a href={`/users/${itemAtual}`}>
                       <img src={`https://github.com/${itemAtual}.png`} />
                       <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <ProfileRelationsBoxWrapper>
+            <ul>
+              {comunidades.map((itemAtual) => {
+                return (
+                  <li key={itemAtual.id}>
+                    <a href={`/users/${itemAtual.title}`}>
+                      <img src={itemAtual.image} />
+                      <span>{itemAtual.title}</span>
                     </a>
                   </li>
                 );
